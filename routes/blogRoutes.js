7a939f0a80a2d7087736b371d66a7026d0eae24f
sushantdhende <sdhende1@gmail.com>
2023-09-14 13:14:15 +0530

    changes part 1

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,16 +1,41 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 
-const Blog = mongoose.model('Blog');
+// const Blog = mongoose.model('Blog');
+const Blog = [
+  {_user: 1},
+  {_user: 2},
+  {_user: 3},
+  {_user: 4},
+  {_user: 5},
+  {_user: 6},
+];
 
 module.exports = app => {
   app.get('/api/blogs/:id', requireLogin, async (req, res) => {
-    const blog = await Blog.findOne({
-      _user: req.user.id,
-      _id: req.params.id
-    });
+    const redis = require('redis');
+    const redisUrl = 'redis://127.0.0.1:6379';
+    const client = redis.createClient(redisUrl);
+    const util = require('util');
+    client.get = util.promisify(client.get);
 
+    const cachedBlogs = await client.get(req.user.id);
+
+    if (cachedBlogs) {
+      console.log('SERVING FROM CACHE');
+      return res.send(JSON.parse(cachedBlogs));
+    }
+
+    const blogs = await Blog.find({ _user: req.user.id });
+
+    // const blog = await Blog.findOne({
+    //   _user: req.user.id,
+    //   _id: req.params.id
+    // });
+
+    console.log('SERVING FROM Mongo');
     res.send(blog);
+    client.set(req.user.id, JSON.stringify(blogs));
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
